c2b7017a0796e236834334776689fc5399b2caa5
Juanco <juanffo874@gmail.com>
2025-01-04 23:31:27 -0600

    Evita que se registren usuarios duplicados

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -1,6 +1,16 @@
 import Veterinario from "../models/Veterinario.js"
 
 const registrar = async (req, res) => {
+    const {email} = req.body
+
+    //Prevenir los usuario duplicados
+    const existeUsuario = await Veterinario.find({email})
+
+    if(existeUsuario){
+        const error = new Error("El usuario ya existe")
+        return res.status(400).json({msg: error.message})
+    }
+
     try {
         //Guardar un nuevo veterinario
         const veterinario = new Veterinario(req.body)
