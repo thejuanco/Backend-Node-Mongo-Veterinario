58cc2511cfc4bf05d8dcfe5683f915a928dccda5
Juanco <juanffo874@gmail.com>
2025-01-04 23:25:41 -0600

    Almacena los nuevos registros en la base de datos

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -1,5 +1,15 @@
-const registrar = (req, res) => {
-    res.send('Ruta principal del veterinario')
+import Veterinario from "../models/Veterinario.js"
+
+const registrar = async (req, res) => {
+    try {
+        //Guardar un nuevo veterinario
+        const veterinario = new Veterinario(req.body)
+        const veterinarioGuardado = await veterinario.save()
+
+        res.status(201).json(veterinarioGuardado)
+    } catch (error) {
+        console.error(error);
+    }
 }
 const perfil = (req, res) => {
     res.send('Ruta principal del perfil')
--- routes/veterinario.routes.js
+++ routes/veterinario.routes.js
@@ -4,7 +4,7 @@ import { registrar, perfil } from '../controllers/veterinarioController.js';
 
 const router = express.Router();
 
-router.get('/', registrar)
+router.post('/', registrar)
 router.get('/perfil', perfil)
 
 export default router
