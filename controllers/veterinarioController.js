0231ff19b8febba534804f4923e1b34d53dbd934
Juanco <juanffo874@gmail.com>
2025-01-05 19:48:55 -0600

    Guarda el token del usuario

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -25,7 +25,27 @@ const perfil = (req, res) => {
     res.send('Ruta principal del perfil')
 }
 
+const confirmar = async (req, res) => {
+    const { token } = req.params;
+    const usuarioConfirmar = await Veterinario.findOne({ token })
+
+    if(!usuarioConfirmar){
+        const error = new Error("Token inválido")
+        return res.status(400).json({msg: error.message})
+    }
+
+    try {
+        usuarioConfirmar.token = null;
+        usuarioConfirmar.confirmado = true
+        await usuarioConfirmar.save()
+        console.log(usuarioConfirmar, 'Usario confirmado')
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 export {
     registrar,
-    perfil
+    perfil,
+    confirmar
 }
