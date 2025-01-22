93c41a12d725673909b4c0adb81cd0ffa36284fd
Juanco <juanffo874@gmail.com>
2025-01-21 21:10:16 -0600

    Crea las rutas protegidas

--- routes/veterinario.routes.js
+++ routes/veterinario.routes.js
@@ -1,12 +1,14 @@
 //Contiene las rutas del veterinario
 import express from 'express';
 import { registrar, perfil, confirmar, autenticar } from '../controllers/veterinarioController.js';
+import checkAuth from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/', registrar)
-router.get('/perfil', perfil)
 router.get('/confirmar/:token', confirmar)
 router.post('/login', autenticar)
+
+router.get('/perfil', checkAuth, perfil)
 
 export default router
