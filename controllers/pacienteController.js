699f4470d7cf83ed3f2b493b0d61e550d8ecece9
Juanco <juanffo874@gmail.com>
2025-02-03 20:28:06 -0600

    Actualiza el paciente

--- controllers/pacienteController.js
+++ controllers/pacienteController.js
@@ -12,12 +12,61 @@ const agregarPaciente = async (req, res) => {
     }
 }
 
-const obtenerPaciente = async (req, res) => {
+const obtenerPacientes = async (req, res) => {
     const pacientes = await Paciente.find().where('veterinario').equals(req.veterinario);
     res.json(pacientes);
 }
 
+const obtenerPaciente = async (req, res) => {
+    const { id } = req.params;
+    const paciente = await Paciente.findById(id);
+
+    if(!paciente){
+        return res.status(404).json({msg: "No encontrado"})
+    }
+    
+    if(paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+        return res.json({msg: "Acción no válida"})
+    }
+
+    if(paciente){
+        res.json(paciente)
+    }
+}
+
+const actualizarPaciente = async (req, res) => {
+    const { id } = req.params;
+    const paciente = await Paciente.findById(id);
+
+    if(!paciente){
+        return res.status(404).json({msg: "No encontrado"})
+    }
+
+    if(paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+        return res.json({msg: "Acción no válida"})
+    }
+
+    //Actualizar paciente
+    paciente.nombre = req.body.nombre || paciente.nombre;
+    paciente.propietario = req.body.propietario || paciente.propietario;
+    paciente.email = req.body.email || paciente.email;
+    paciente.fecha = req.body.fecha || paciente.fecha;
+    paciente.sintomas = req.body.sintomas || paciente.sintomas;
+
+    try {
+        const pacienteActualizado = await paciente.save()
+        res.json(pacienteActualizado)
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+const eliminarPaciente = async (req, res) => {}
+
 export {
     agregarPaciente,
-    obtenerPaciente
+    obtenerPacientes,
+    actualizarPaciente,
+    obtenerPaciente,
+    eliminarPaciente
 }
--- routes/paciente.routes.js
+++ routes/paciente.routes.js
@@ -1,12 +1,24 @@
 import express from "express";
 import { 
     agregarPaciente,
-    obtenerPaciente 
+    obtenerPacientes,
+    obtenerPaciente,
+    actualizarPaciente,
+    eliminarPaciente
 } from "../controllers/pacienteController.js";
 import checkAuth from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").post(checkAuth, agregarPaciente).get(checkAuth, obtenerPaciente);
+router
+  .route("/")
+  .post(checkAuth, agregarPaciente)
+  .get(checkAuth, obtenerPacientes);
+
+router
+  .route("/:id")
+  .get(checkAuth, obtenerPaciente)
+  .put(checkAuth, actualizarPaciente)
+  .delete(checkAuth, eliminarPaciente);
 
 export default router;
